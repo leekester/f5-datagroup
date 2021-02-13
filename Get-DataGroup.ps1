6786ea00269d8b6752ddc3e7e9b1abcea2b5e0b9
Matt Leeke <matthewleeke@hastingsdirect.com>
2021-02-13 11:33:32 +0000

    Added code to ignore untrusted certificates

--- Get-DataGroup.ps1
+++ Get-DataGroup.ps1
@@ -17,5 +17,42 @@ $Headers = @{
     Authorization = $basicAuthValue
 }
 
+# Uncomment this if you have untrusted certificates...
+
+if (-not ([System.Management.Automation.PSTypeName]'ServerCertificateValidationCallback').Type)
+{
+$certCallback = @"
+    using System;
+    using System.Net;
+    using System.Net.Security;
+    using System.Security.Cryptography.X509Certificates;
+    public class ServerCertificateValidationCallback
+    {
+        public static void Ignore()
+        {
+            if(ServicePointManager.ServerCertificateValidationCallback ==null)
+            {
+                ServicePointManager.ServerCertificateValidationCallback += 
+                    delegate
+                    (
+                        Object obj, 
+                        X509Certificate certificate, 
+                        X509Chain chain, 
+                        SslPolicyErrors errors
+                    )
+                    {
+                        return true;
+                    };
+            }
+        }
+    }
+"@
+    Add-Type $certCallback
+ }
+
+[System.Net.ServicePointManager]::SecurityProtocol = [System.Net.SecurityProtocolType]::Tls12;
+[ServerCertificateValidationCallback]::Ignore()
+
+
 # === CONNECT TO F5 API =========
 $reply = Invoke-RestMethod -Uri https://${f5server}/mgmt/tm/ltm/pool/~${f5partition}~${f5poolname}/members/~${f5partition}~${servername}:${f5port}/stats/?$select=serverside.curConns -Headers $Headers
