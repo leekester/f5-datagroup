4da3cc070ad4a172452d79d999c758ae36b276b8
Matt Leeke <matthewleeke@hastingsdirect.com>
2021-02-13 11:21:28 +0000

    Initial rough commit to bring code into lab environment

---
+++ Get-DataGroup.ps1
@@ -0,0 +1,21 @@
+﻿# Set variables...
+$f5partition = "Production"
+$f5poolname = "Frontend_App_pool"
+$f5port = 8080
+$user = "myf5userid"
+$pass = "myf5password"
+$f5server = "myf5server.mydomain.local"
+
+# ======= NO CHANGES BELOW THIS LINE! =======
+$servername = $env:COMPUTERNAME
+
+# ====== AUTHORISATION SECTION ==========
+$pair = "$($user):$($pass)"
+$encodedCreds = [System.Convert]::ToBase64String([System.Text.Encoding]::ASCII.GetBytes($pair))
+$basicAuthValue = "Basic $encodedCreds"
+$Headers = @{
+    Authorization = $basicAuthValue
+}
+
+# === CONNECT TO F5 API =========
+$reply = Invoke-RestMethod -Uri https://${f5server}/mgmt/tm/ltm/pool/~${f5partition}~${f5poolname}/members/~${f5partition}~${servername}:${f5port}/stats/?$select=serverside.curConns -Headers $Headers
