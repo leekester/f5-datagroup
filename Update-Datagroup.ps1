9e1ccd2e1d420a6423c149b55d4529b086153aa7
Matt Leeke <matthewleeke@hastingsdirect.com>
2021-02-13 23:29:43 +0000

    Adding code to update datagroup. This currently just constructs the JSON body

---
+++ Update-Datagroup.ps1
@@ -0,0 +1,13 @@
+﻿# curl -k -u admin:f5rules -H "Content-Type: application/json" -X PUT -d '{"records":[{"name":"123","data":"435" }]}' https://1.0.1.6/mgmt/tm/ltm/data-group/internal/~Common~TEST_iRule
+
+$Template = '{"records":[]}' | ConvertFrom-Json
+
+ForEach ($IP in $MailRelayIPs) {
+    $JsonDataAdd = @"
+    {
+        "name": "$IP",
+        "data": ""
+    }
+"@
+    $Template.records += (ConvertFrom-Json $JsonDataAdd)
+    }
